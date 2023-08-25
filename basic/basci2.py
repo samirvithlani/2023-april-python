6d3da1125dbe0beca6b01e85db84daa23ef330c7
samirvithlani <samir.vithlani83955@gmail.com>
2023-08-25 18:42:54 +0530

    bacis

---
+++ basic/basci2.py
@@ -0,0 +1,13 @@
+
+#no = input("Enter a number: ")
+#int(str)
+# int a = 12.2
+# 12
+no = int(input("Enter a number: "))
+print("value of no is ",no)
+
+print("type of no ",type(no))
+
+per = float(input("Enter a percentage: "))
+print("value of per is ",per)
+print("type of per ",type(per))
---
+++ basic/basic1.py
@@ -0,0 +1,29 @@
+
+print("Hello World!")
+a = 100
+print("value of a is ",a)
+print("type of a ",type(a))
+
+f = 3.14
+print("value of f is ",f)
+print("type of f ",type(f))
+
+name = "Python"
+print("value of name is ",name)
+print("type of name ",type(name))
+
+c = 'A'
+print("value of c is ",c) 
+print("type of c ",type(c))
+
+isActive = False
+print("value of isActive is ",isActive)
+print("type of isActive ",type(isActive))
+
+#comment 
+# c lang // single line comment   # single line comment
+'''
+    multi line comment
+    /*
+    s*/
+'''
