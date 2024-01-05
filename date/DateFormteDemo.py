df66abcab9935b1631d3e09c4a9d6667a5264cbd
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-05 18:55:16 +0530

    tz

--- date/DateFormteDemo.py
+++ date/DateFormteDemo.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import pytz
 
-now = datetime.now()
+now = datetime.now(pytz.timezone("Europe/London"))
 print(now)
 
 b1 = now.strftime("%d/%m/%Y %H:%M:%S")
@@ -8,7 +9,7 @@ print("b1 =", b1)
 
 #12hour format
 
-b1 = now.strftime("%d/%m/%Y %I:%M:%S %p %A %Z")
+b1 = now.strftime("%d/%m/%Y %I:%M:%S %p %A %z %Z")
 print("b1 =", b1)
 
 #week Day.. print day name
